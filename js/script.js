82a3e30f187b27ee6873999ed62e4a2fd2584b05
Jarek.Mankiewicz <mmagari@wp.pl>
2023-04-29 21:15:13 +0200

    Add rest of js for function titleClickHandler

--- js/script.js
+++ js/script.js
@@ -9,20 +9,24 @@ for (let activeLink of activeLinks){
   activeLink.classList.remove('active');
 }
 /*add class 'active' to the clicked link*/
+event.preventDefault();
 const clickedElement = this;
 console.log('clickedElement:', clickedElement);
 clickedElement.classList.add('active');
-//console.log('clickedElement (withplus): ' +clickedElement);
 /*remove class 'active' fromm all articles*/
 const activeArticles = document.querySelectorAll('.posts article.active');
 for (let activeArticle of activeArticles){
   activeArticle.classList.remove('active');
 }
-}
 /*get 'href' attribute from the clicked link*/
+const articleSelector = clickedElement.getAttribute('href');
+console.log('My link:', articleSelector);
 /*find the correct article using the selector (value of 'href' attribute)*/
+const targetArticle = document.querySelector(articleSelector);
+console.log('My article:', targetArticle);
 /*add class 'active' to the correct article*/
-
+targetArticle.classList.add('active');
+}
 const links = document.querySelectorAll('.titles a');
 
 for(let link of links){
